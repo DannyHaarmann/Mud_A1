2d4b2186fc4d00b92cfb1d58351372d015202348
DannyHaarmann <94137078+DannyHaarmann@users.noreply.github.com>
2022-04-08 11:51:21 -0500

    new textures

---
+++ MudMan/Assets/Scripts/CORE.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CORE : MonoBehaviour
+{
+    public Transform enemy;
+    public Transform spawnpont;
+    // Start is called before the first frame update
+    public static void 
+    void Start()
+    {
+        CORE.theRooms.add(go);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
