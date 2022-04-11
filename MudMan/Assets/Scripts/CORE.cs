1dc4a8aedb3ba3c47be69fa03962e171e2e8daa4
DannyHaarmann <94137078+DannyHaarmann@users.noreply.github.com>
2022-04-11 10:33:37 -0500

    rando move

--- MudMan/Assets/Scripts/CORE.cs
+++ MudMan/Assets/Scripts/CORE.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 
 public class CORE : MonoBehaviour
 {
-    public Transform enemy;
-    public Transform spawnpont;
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+
+    private static List<GameObject> theRooms = new List<GameObject>();
+
+    public static void addRoomGO(GameObject go)
+    {
+        CORE.theRooms.Add(go);
+        print("Added Room");
+    }
+
+    public static void display()
+    {
+        print("Woot");
+    }
+
     // Start is called before the first frame update
-    public static void 
     void Start()
     {
-        CORE.theRooms.add(go);
+        for (int i = 0; i < 20; i++)
+        {
+            Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity);
+        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
--- MudMan/Assets/Scripts/Enemey.cs
+++ MudMan/Assets/Scripts/Enemey.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemey : MonoBehaviour
+public class Enemey  : MonoBehaviour
 {
+    private int count;
+    private Rigidbody rb;
+
     // Start is called before the first frame update
-    void Start()
+    void Start() //like a constructor
     {
-        
+        rb = this.gameObject.GetComponent<Rigidbody>();
+
+        count = 0;
+
+        rb.velocity = new Vector3(Random.Range(-6, 6), 0, Random.Range(-6, 6));
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            CORE.display();
+            count++;
+            if (count == 3)
+            {
+                this.gameObject.SendMessage("DoSomething");
+                Destroy(this.gameObject);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
