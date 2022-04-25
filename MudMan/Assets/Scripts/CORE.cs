31b448fdd2f5e9881436986ea3aa1fbaa27f1b6d
DannyHaarmann <94137078+DannyHaarmann@users.noreply.github.com>
2022-04-25 10:23:18 -0500

    edit scripts

--- MudMan/Assets/Scripts/CORE.cs
+++ MudMan/Assets/Scripts/CORE.cs
@@ -2,37 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class CORE : MonoBehaviour
 {
     public Transform enemyPrefab;
     public Transform spawnPoint;
 
-    private static List<GameObject> theRooms = new List<GameObject>();
+    private static List<GameObject> theRoomsGos = new List<GameObject>();
+    private static List<Room> theRooms = new List<Room>();
+    private static Player currentPlayer = null;
+
+    public static void setPlayer(Player p)
+    {
+        CORE.currentPlayer = p;
+    }
+
+    public static Player getPlayer()
+    {
+        return CORE.currentPlayer;
+    }
+
+    public static void addRoom(Room theRoom)
+    {
+        CORE.theRooms.Add(theRoom);
+    }
 
     public static void addRoomGO(GameObject go)
     {
-        CORE.theRooms.Add(go);
-        print("Added Room");
+        CORE.theRoomsGos.Add(go);
+        //print("Added Room");
     }
 
     public static void display()
     {
-        print("Woot");
-    }
 
+    }
     // Start is called before the first frame update
     void Start()
     {
-           // for (int i = 0; i < 20; i++)
+        /*
+        for (int i = 0; i < 100; i++)
         {
-            Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity);
+      
+            Transform t = Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity);
+            Rigidbody rb = t.GetComponent<Rigidbody>();
+            rb.velocity = new Vector3(Random.Range(10, 30), Random.Range(0, 20), Random.Range(10, 30));
         }
-        
+        */
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
